5091028ffb04e0ae7542e46fd1d4ba54f67041dc
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-04 18:19:13 +0600

    delete product with api

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -10,13 +10,30 @@ import {product} from "../models/data.type";
 export class SellerHomeComponent implements OnInit{
 
   productList:undefined | product[];
+  productDeleteMessage: undefined | string;
   constructor(private productService:ProductService) {
   }
 
   ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts(){
     this.productService.getAllProducts().subscribe((result)=>{
       console.warn(result);
       this.productList = result;
     });
+  }
+
+  deleteProduct(product: product) {
+    this.productService.deleteProduct(product.id).subscribe((result)=>{
+      if (result){
+        this.productDeleteMessage="Product is deleted successfully";
+        this.getAllProducts();
+      }
+    })
+    setTimeout(()=>{
+      this.productDeleteMessage=undefined;
+    }, 3000)
   }
 }
