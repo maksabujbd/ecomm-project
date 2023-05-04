c8f705e082211bf73467ec363dd1ac4ad87bc984
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-04 17:02:54 +0600

    logout and seller pages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,7 @@ import {Router} from "@angular/router";
 export class HeaderComponent implements OnInit {
 
   menuType = 'default';
+  sellerName='';
 
   constructor(private route: Router) {
   }
@@ -20,12 +21,22 @@ export class HeaderComponent implements OnInit {
         if (localStorage.getItem('seller') && value.url.includes('seller')) {
           console.warn('in seller area');
           this.menuType = "seller";
+          if (localStorage.getItem('seller')){
+            let sellerLocalstorage = localStorage.getItem('seller');
+            let sellerData = sellerLocalstorage && JSON.parse(sellerLocalstorage)[0];
+            this.sellerName = sellerData.name
+          }
         } else {
           console.warn('outside seller');
           this.menuType='default';
         }
       }
     })
+  }
+
+  logout(){
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']).then();
   }
 
 }
