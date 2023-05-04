119699fb05fcd875322bbc2661f87c3720410246
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-04 18:30:53 +0600

    add font awesome for icon

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,39 +1,42 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../services/product.service";
 import {product} from "../models/data.type";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent implements OnInit{
+export class SellerHomeComponent implements OnInit {
 
-  productList:undefined | product[];
+  productList: undefined | product[];
   productDeleteMessage: undefined | string;
-  constructor(private productService:ProductService) {
+  icon = faTrash;
+
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit(): void {
     this.getAllProducts();
   }
 
-  getAllProducts(){
-    this.productService.getAllProducts().subscribe((result)=>{
+  getAllProducts() {
+    this.productService.getAllProducts().subscribe((result) => {
       console.warn(result);
       this.productList = result;
     });
   }
 
   deleteProduct(product: product) {
-    this.productService.deleteProduct(product.id).subscribe((result)=>{
-      if (result){
-        this.productDeleteMessage="Product is deleted successfully";
+    this.productService.deleteProduct(product.id).subscribe((result) => {
+      if (result) {
+        this.productDeleteMessage = "Product is deleted successfully";
         this.getAllProducts();
       }
     })
-    setTimeout(()=>{
-      this.productDeleteMessage=undefined;
+    setTimeout(() => {
+      this.productDeleteMessage = undefined;
     }, 3000)
   }
 }
