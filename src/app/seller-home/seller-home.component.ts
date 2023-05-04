6f21c15dc125e066af376c4e784677e35ea67109
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-04 18:01:04 +0600

    product list with api

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {product} from "../models/data.type";
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent implements OnInit{
 
+  productList:undefined | product[];
+  constructor(private productService:ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe((result)=>{
+      console.warn(result);
+      this.productList = result;
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {product} from "../models/data.type";
 
@@ -7,9 +7,15 @@ import {product} from "../models/data.type";
 })
 export class ProductService {
 
-  constructor(private http:HttpClient) { }
-  addProduct(data:product){
+  constructor(private http: HttpClient) {
+  }
+
+  addProduct(data: product) {
     console.warn("product add service called");
     return this.http.post('http://localhost:3000/products', data);
+  }
+
+  getAllProducts() {
+    return this.http.get<product[]>('http://localhost:3000/products');
   }
 }
