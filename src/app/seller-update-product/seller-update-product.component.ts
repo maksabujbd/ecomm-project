50f5c158d6a5717dd8034f17fc3bb01ade610614
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-04 19:33:05 +0600

    propulate update product form

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ProductService} from "../services/product.service";
+import {product} from "../models/data.type";
 
 @Component({
   selector: 'app-seller-update-product',
   templateUrl: './seller-update-product.component.html',
   styleUrls: ['./seller-update-product.component.css']
 })
-export class SellerUpdateProductComponent {
+export class SellerUpdateProductComponent implements OnInit {
+
+  productData: undefined | product;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
+  }
 
   submit(value: any) {
 
+  }
+
+  ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    console.warn(productId);
+    productId && this.productService.getProduct(productId).subscribe((result) => {
+      console.warn(result);
+      this.productData = result;
+    })
   }
 }
