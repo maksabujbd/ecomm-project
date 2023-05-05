87191b9a447ee02334bd6e153ba530a5f57e54ea
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-05 11:59:09 +0600

    search-api

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {ProductService} from "../services/product.service";
+import {product} from "../models/data.type";
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  searchResult : undefined| product[];
+  constructor(private activatedRoute: ActivatedRoute,
+              private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    const query = this.activatedRoute.snapshot.paramMap.get('query');
+    console.warn(query);
+    query && this.productService.searchProducts(query).subscribe((result)=>{
+      this.searchResult = result;
+    });
   }
 }
